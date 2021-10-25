104198890de29db34c7d2ed896c0c2bc3f024667
4Orange2 <92903179+4Orange2@users.noreply.github.com>
2021-10-24 20:05:46 -0400

    Added steering_angle, arm_movement, and gyro_straight_distance (#7)
    Also added some docstrings and minor improvements.

--- blocks.py
+++ blocks.py
@@ -1,33 +1,78 @@
 # This is where we keep all the shared blocks. Please only include functions
 # and constants in this file for now.
+import time
 
-SHARPNESS = 0.7
-SPEED = 300
+
+SHARPNESS = 3
+SHARPNESS_COLOR = 0.7 
+SPEED = 100
 WHEEL_CIRCUMFERENCE = 180
+TURN_SPEED = 45
+
 
 def follow_line(tank, left_cs, right_cs, millies):
-    tank.reset()
-    while tank.distance() < millies:
-        subtract = left_cs.reflection() - right_cs.reflection() # subtracts color sensors
-        multiply = subtract * SHARPNESS # multiplies the difference by 0.7
-        tank.drive(SPEED, multiply) # follows the line 
+    ''' Follows the line for a certain distance
 
-def gyro_straight(tank, gyro, millies, target):
+    Args:
+        millies: the amount of millilmeters the robot should travel
+    '''
     tank.reset()
     while tank.distance() < millies:
-        subtract = gyro.angle() - target  # subtracts color sensors
-        multiply = subtract * SHARPNESS # multiplies the difference by 0.7
-        tank.drive(SPEED, multiply) # follows the line
-        
-        
-        
-def gyro_angle( tank, gyro_sensor, left_wheel, right_wheel, angle):
-    gyro_sensor.reset_angle(0)
+        subtract = left_cs.reflection() - right_cs.reflection() 
+        multiply = subtract * SHARPNESS_COLOR
+        tank.drive(SPEED, multiply) 
+    tank.stop()
+
+
+def gyro_straight(tank, gyro, left_cs, right_cs, target):
+    ''' Goes forward until it reaches black trying to keep gyro angle straight.
+
+    Args:
+        target: The amount of going forward in
+    '''
+    tank.settings(200, 100, 30, 30)
+    while not stop_on_black(left_cs, right_cs): 
+        subtract = gyro.angle() - target  
+        multiply = subtract * (SHARPNESS * -1) 
+        tank.drive(SPEED, multiply)
+        print(gyro.angle())
+    tank.stop() 
+
+
+def gyro_straight_distance(tank, gyro, millies, target):
+    ''' Goes forward for a certain distance trying to keep gyro angle straight. 
+
+    Args: 
+        millies: how much millimeters the robot should travel
+    '''
+    tank.settings(200, 100, 30, 30)
+    tank.reset()
+    while tank.distance() < millies:
+        subtract = gyro.angle() - target 
+        multiply = subtract * (SHARPNESS * -1) 
+        tank.drive(SPEED, multiply)
+        print(gyro.angle())
+    tank.stop()
+
+
+def arm_movement(forklift, speed, millies):
+    ''' Makes the robot arm go up/down for a certain amount of millimeters
+
+    Args: 
+        millies: the amount of millimeters the arm goes up/down  
+        speed: the amount of speed the arm travels with 
+    ''' 
+    degrees = millies * 10  
+    forklift.run_angle(speed, degrees)
+
+
+def gyro_angle( tank, gyro, left_wheel, right_wheel, angle):
+    gyro.reset_angle(0)
     big = angle + 1
     small = angle - 1
     while gyro.angle() <= small or gyro.angle() >= big:
         while gyro.angle() <= small or gyro.angle() >= big: 
-            if gyro_sensor.angle() <= small:
+            if gyro.angle() <= small:
                 left_wheel.run(TURN_SPEED)
                 right_wheel.run(TURN_SPEED * -1)
             else:
@@ -35,18 +80,24 @@ def gyro_angle( tank, gyro_sensor, left_wheel, right_wheel, angle):
                 right_wheel.run(TURN_SPEED)
         tank.stop()
         time.sleep(0.5)
-        #print(gyro.angle())
-        
-        
-        
-def stop_on_black(left_cs, right_cs):
-        if left_cs.reflection() <= 14 and right_cs.reflection() <= 14:
-            return True
-        else:
-            return False
+        print(gyro.angle())
 
-        
-def perpendicular_line(tank, left_cs, right_cs, line_distance):
+def stop_on_black(left_cs, right_cs):
+    ''' Used so the robot can identify black
+    '''
+    if left_cs.reflection() <= 9 and right_cs.reflection() <= 9:
+        return True
+    else:
+        return False
+
+
+def perpendicular_line(tank, left_cs, right_cs, line_distance, left_wheel, right_wheel):
+    ''' The robot goes to an intersection of lines to create a reference point
+
+    Args:
+        line_distance: the distance of your end-point 
+            on the line from the intersection of the two lines
+    '''
     if line_distance >= 0:
         turn = 85
     else:
@@ -66,3 +117,10 @@ def steering(tank, speed, sharpness, millies):
     while tank.distance() >= small and tank.distance() <= big:
         tank.drive(speed, sharpness)
     tank.stop()   
+
+def steering_angle(tank, gyro, speed, sharpness, angle):
+    big = angle + 1
+    small = angle - 1
+    while gyro.angle() <= small or gyro.angle() >= big:
+        tank.drive(speed, sharpness)
+    tank.stop()
