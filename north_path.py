a17a4b4238083a7a7b90fbb4c2397403f55c2178
4Orange2 <92903179+4Orange2@users.noreply.github.com>
2021-11-14 21:20:46 -0500

    Added sorting center

--- north_path.py
+++ north_path.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env pybricks-micropython
 ''' The north path
 
 This progrma does the north path in the following order:
@@ -11,6 +12,7 @@ This progrma does the north path in the following order:
 
 import robot
 import time
+import blocks
 
 
 my_robot = robot.Robot()
@@ -30,34 +32,58 @@ my_robot.straight_distance(-75)
 
 # maneuvers to hit the airplane
 my_robot.arm_movement(-200, 20)
-my_robot.steering(-1000, 19.3, 394)
+my_robot.steering(-2000, 18.5, 335) # initial value = 376
 # airplane done
 
 # preparing for the ship 
-my_robot.steering_angle(speed=40, sharpness=-6, angle=2)
+my_robot.steering_angle(speed=40, sharpness=16, angle=0)
 print('angle after plane: ' + str(my_robot.gyro.angle()))
-my_robot.gyro_angle(2)
-my_robot.straight_distance(380)
+my_robot.gyro_angle(0)
+my_robot.straight_distance(350)
 my_robot.straight_to_black(speed=100, ignore_left=True)
-# got to black line
+# got to bridge black line
 my_robot.arm_movement(speed=200, millies=25)
-my_robot.straight_distance(30)
-my_robot.gyro_angle(-89)
+my_robot.straight_distance(distance=45, speed=100)
+my_robot.gyro_angle(-88)
 my_robot.straight_to_black(speed=100, ignore_right=True)
 my_robot.brake()
-my_robot.follow_line(105, speed=75)
+my_robot.follow_line(115, speed=75)
 # at ship
-my_robot.straight_distance(2.25)
-my_robot.arm_movement(speed=200, millies=17)
-time.sleep(0.3)
-my_robot.straight_distance(distance=-130, speed=50)
+my_robot.straight_distance(3.6)
+my_robot.arm_movement(speed=200, millies=13)
+time.sleep(0.8)
+my_robot.straight_distance(distance=-115, speed=17)
 # ship done
 my_robot.arm_movement(speed=-300, millies=125)
 my_robot.straight_distance(60)
-my_robot.gyro_angle(10)
-my_robot.gyro_straight_distance(millies=260, target_angle=10, speed=100)
+my_robot.gyro_angle(2)
+my_robot.gyro_straight_distance(distance=140, target_angle=2, speed=50)
+my_robot.gyro_angle(2)
+my_robot.gyro_straight_distance(distance=90, target_angle=2, speed=50)
 # crane done
-my_robot.straight_distance(-20)
-my_robot.gyro_angle(12.3)
-my_robot.straight_distance(distance=-550, speed=1000)
+my_robot.straight_distance(-40)
+my_robot.gyro_angle(2)
+my_robot.straight_distance(distance=-495, speed=2000)
 # truck done
+
+# maneuver for reference point
+my_robot.straight_distance(200)
+my_robot.gyro_angle(90)
+my_robot.straight_to_black(speed=100)
+my_robot.straight_distance(-20)
+# at bridge black line
+my_robot.steering_angle(speed=75, sharpness=50, angle=4)
+my_robot.gyro_angle(4)
+my_robot.gyro_straight_distance(distance=170, target_angle=4)
+my_robot.straight_to_black(speed=100, ignore_left=True)
+# at sorting center loose end
+my_robot.straight_distance(130)
+my_robot.gyro_angle(90)
+my_robot.follow_line(distance=130, speed=70, sharpness_color=0.9)
+my_robot.straight_to_black(ignore_right=True, speed=100)
+my_robot.gyro.reset_angle(90)
+# at sorting center
+my_robot.straight_distance(-50)
+my_robot.gyro_angle(2)
+my_robot.straight_distance(-50)
+my_robot.arm_movement(9)
