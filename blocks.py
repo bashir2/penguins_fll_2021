66c88882729e0b796bba78d1bf4b141023b4de49
4Orange2 <92903179+4Orange2@users.noreply.github.com>
2021-11-28 16:10:02 -0500

    Tuning go_to_bay, blue_circle, and train

--- blocks.py
+++ blocks.py
@@ -13,27 +13,26 @@ def go_to_bay(r, bay_num, init_distance, is_blue=False):
     '''
     print('go_to_bay {}'.format(bay_num))
     if bay_num == 1:
-        r.straight_distance(14 - init_distance) 
+        r.straight_distance(distance=11 - init_distance) # init value = 14
         r.gyro_angle(90)
     elif bay_num == 2:
-        r.straight_distance(130 - init_distance)
+        r.straight_distance(distance=120 - init_distance) # init value = 130
         r.gyro_angle(90)
     elif bay_num == 3:
-        r.straight_distance(169 - init_distance)
-        r.gyro_angle(90) 
+        r.straight_distance(distance=180 - init_distance) # init value = 194
+        r.gyro_angle(90)
     else:
         print('ERROR bay_num should be in [1,3] got {}'.format(bay_num))
-    d = 0
-    forward = 0
+    backward = -80
+    forward = 88
     if is_blue:
-        d = 80
-        forward = 25
-    r.straight_distance(speed=80, distance=-80 + d)
+        backward = 0
+        forward = 80
+    r.straight_distance(speed=80, distance=backward)
     r.arm_movement(speed=400, millies=80)
-    r.straight_distance(92 - forward) # init value = 86
+    r.straight_distance(forward) # init value = 86
     r.arm_movement(speed=-400, millies=45)
-    r.straight_distance(distance=-10, speed=100)
-    return bay_num
+    r.straight_distance(distance=-10, speed=80)
 
 def check_cargo(r):
     '''
@@ -56,9 +55,9 @@ def check_cargo(r):
     print('distance of the third cargo: ' + str(cargo3))
     # does train track
     r.straight_distance(speed=90, distance=17)
-    r.arm_movement(speed=400, millies=127)  # arm at height: 63
+    r.arm_movement(speed=600, millies=127)  # arm at height: 63
     r.straight_distance(speed=90, distance=-26)
-    r.arm_movement(speed=-400, millies=57, wait=False)  # arm at height: 120
+    r.arm_movement(speed=-600, millies=57, wait=False)  # arm at height: 120
     # At this point we are 144 mm from reference line
 
     if cargo1 < cargo2 and cargo1 < cargo3:
@@ -99,15 +98,15 @@ def cargo_connect(r, green_loc):
         r.gyro_angle(175)
         r.straight_to_black()
     else:
-        r.steering_angle(speed=-100, sharpness=20, angle=48)
-        r.steering_angle(speed=100, sharpness=66, angle=175)
+        r.steering_angle(speed=-100, sharpness=40, angle=38)
+        r.steering_angle(speed=100, sharpness=86, angle=175)
         r.straight_to_black(speed=100)
-        r.gyro_angle(175)
-    r.straight_distance(100)
-    r.arm_movement(speed=400, millies=56)
+        r.gyro_angle(177)
+    r.straight_distance(80)
+    r.arm_movement(speed=500, millies=56)
     r.straight_to_black(speed=-100)
     # move arm to height 120
-    r.arm_movement(speed=-400, millies=91)
+    r.arm_movement(speed=-500, millies=91)
 
 
 def blue_circle(r, bay_num):
@@ -118,15 +117,17 @@ def blue_circle(r, bay_num):
     Postconditions:
         Arm at height: 30
     '''
+    angle = 269
     if bay_num == 1:
         r.steering_angle(speed=-150, sharpness=20, angle=120)
     elif bay_num == 2:
         r.straight_distance(-230)
-    else:
-        r.steering_angle(speed=-150, sharpness=20, angle=60)
-    r.gyro_angle(angle=260, speed=100)
-    r.straight_distance(120)
-    r.arm_movement(speed=200, millies=55)
+    else: # this worked consistently
+        r.steering_angle(speed=-150, sharpness=50, angle=0)
+        angle = -75
+    r.gyro_angle(angle=angle, speed=100)
+    r.gyro_straight_distance(distance=150, target_angle=angle, speed=100)
+    r.arm_movement(speed=600, millies=55)
     r.straight_distance(-200)
 
 
@@ -138,9 +139,9 @@ def pick_blue(r, blue_loc):
     Postconditions:
         Arm at height: 30
     '''
-    bay_num = go_to_bay(r, blue_loc, init_distance=0, is_blue=True)
+    go_to_bay(r, blue_loc, init_distance=0, is_blue=True)
     # now arm is at height 85
-    blue_circle(r, bay_num)
+    blue_circle(r, blue_loc)
 
 
 def train(r):
@@ -151,15 +152,14 @@ def train(r):
     Postconditions:
         Arm at height: 190
     '''
-    print('train started')
-    r.arm_movement(speed=-500, millies=160, wait=False)
+    r.arm_movement(speed=-1000, millies=160, wait=False)
     # now arm is at height 190  
-    r.gyro_angle(240)
+    r.gyro_angle(-110)
     r.straight_to_black(speed=80)
-    r.straight_distance(140) # init value = 165
-    r.gyro_angle(330)
-    r.straight_distance(-100)
-    r.follow_line(distance=150, speed=40)
+    r.straight_distance(130) # init value = 165
+    r.gyro_angle(-30)
+    r.straight_distance(-60)
+    r.follow_line(distance=200, speed=40, sharpness_color=0.7)
     r.gyro.reset_angle(-30)
     # fix the angle
     r.straight_to_black(speed=70)
@@ -168,9 +168,8 @@ def train(r):
     r.straight_distance(-85)
     # does helicopter
     r.straight_distance(-60)
-    r.steering_angle(speed=-40, sharpness=50, angle=-170)
-    time.sleep(0.5)
-    r.straight_distance(-86)
-    r.arm_movement(speed=400, millies=63, is_back=True)
+    r.steering_angle(speed=-40, sharpness=40, angle=-160)
+    r.straight_distance(-93)
+    r.arm_movement(speed=400, millies=67, is_back=True)
     # Goes to train from fixed point and puts down the cargo boxes
     r.straight_distance(80)
