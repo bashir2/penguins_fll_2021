df35d702260404b493457fe922bcd40b4669f0c5
4Orange2 <92903179+4Orange2@users.noreply.github.com>
2021-11-28 16:14:04 -0500

    final tuning of north_path

--- north_path.py
+++ north_path.py
@@ -8,6 +8,9 @@ This program does the north path in the following order:
 4. Ship
 5. Crane
 6. Truck
+7. Sorting center
+8. Helicopter
+9. Train
 '''
 
 import robot
@@ -18,37 +21,36 @@ import blocks
 r = robot.Robot()
 
 # Write your program here.
-
 # follow the line to push the truck and the bridge
-r.gyro_angle(-54) 
-r.straight_distance(280)
-r.follow_line(510)
-r.arm_movement(-400, 52)
+r.gyro_angle(-54)
+r.straight_distance(distance=280)
+r.follow_line(510, sharpness_color=0.46)
+r.arm_movement(-800, 52)
 # truck and first bridge done 
-r.straight_distance(190) # init value = 200
-r.arm_movement(400, 32)
+r.straight_distance(distance=190) # init value = 200
+r.arm_movement(800, 32)
 r.straight_distance(-75)
 # second bridge done
 
 # maneuvers to hit the airplane
 r.arm_movement(-400, 20)
-r.steering(-2000, 18.5, 310)
+r.steering(-1200, 18.5, 310)
 # airplane done
 
 # preparing for the ship 
 r.gyro_angle(0)
 print('angle after plane: ' + str(r.gyro.angle()))
 r.gyro_angle(0)
-r.straight_distance(380)
-r.straight_to_black(speed=100, ignore_right=True)
+r.gyro_straight_distance(distance=380, target_angle=0)
+r.straight_to_black(speed=100, ignore_left=True)
 # got to bridge black line
 r.arm_movement(speed=400, millies=25, wait=False)
-r.straight_distance(distance=45, speed=100)
+r.straight_distance(distance=55, speed=100)
 r.gyro_angle(-90)
 r.straight_to_black(speed=100, ignore_right=True)
 r.follow_line(115, speed=75)
 # at ship
-r.straight_distance(3.26)
+r.straight_distance(distance=4.26, speed=100)
 r.arm_movement(speed=200, millies=13)
 time.sleep(0.8)
 r.straight_distance(distance=-125, speed=17)
@@ -57,30 +59,30 @@ r.arm_movement(speed=-400, millies=125)
 r.straight_distance(60)
 r.gyro_angle(0)
 r.gyro_straight_distance(distance=140, target_angle=0, speed=50)
-r.gyro_angle(0)
-r.gyro_straight_distance(distance=90, target_angle=0, speed=50)
+r.gyro_angle(-1)
+r.gyro_straight_distance(distance=90, target_angle=-1, speed=50)
 # crane done
 r.straight_distance(-40)
-r.gyro_angle(3) 
-r.gyro_straight_distance(distance=455, speed=-2000, target_angle=3, sharpness=1)
+r.gyro_angle(5) 
+r.gyro_straight_distance(distance=455, speed=-2000, target_angle=5, sharpness=1)
 # truck done
 
 # maneuver for reference point
 r.straight_distance(200)
 r.gyro_angle(90)
 r.straight_to_black(speed=100)
-r.straight_distance(-25)
+r.straight_distance(-20)
 # at bridge black line
-r.steering_angle(speed=75, sharpness=50, angle=3)
-r.gyro_angle(3)
-r.gyro_straight_distance(distance=170, target_angle=2)
+r.steering_angle(speed=75, sharpness=50, angle=4)
+r.gyro_angle(6)
+r.gyro_straight_distance(distance=170, target_angle=6)
 r.straight_to_black(speed=100, ignore_left=True)
 
 
 # at sorting center loose end
-r.straight_distance(125)
+r.straight_distance(135)
 r.gyro_angle(90)
-r.follow_line(distance=130, speed=40, sharpness_color=0.7)
+r.follow_line(distance=130, speed=40, sharpness_color=0.75)
 r.gyro.reset_angle(90)
 r.straight_to_black(ignore_right=True, speed=100)
 # at sorting center
@@ -95,6 +97,7 @@ r.straight_distance(60)
 r.gyro_angle(90)
 r.follow_line(distance=40, speed=40, sharpness_color=0.4)
 r.straight_to_black(ignore_right=True, speed=100)
+r.gyro_angle(90)
 # at sorting center again
 r.straight_distance(-50)
 r.gyro_angle(0)
@@ -102,3 +105,15 @@ r.gyro_angle(0)
 blocks.pick_blue(r, blue_loc)
 # does the train
 blocks.train(r)
+'''
+# goes for the accident avoidance
+r.gyro_straight_distance(speed=500, distance=450, target_angle=-90)
+r.straight_to_black()
+r.gyro_angle(-90)
+r.ev3.speaker.say('hi')
+r.gyro_angle(270)
+r.straight_to_black()
+r.steering(speed=400, sharpness=-55, distance=320)
+r.straight_to_black()
+r.straight_distance(66)
+'''
